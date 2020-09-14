e5c635452e132a1244bf2e619bf15a5396882d02
Sreenivas <ksreenivascse@gmail.com>
2020-09-14 21:36:49 +0530

    Dice roller completed Project

--- DiceRoller - Ios/ViewController.swift
+++ DiceRoller - Ios/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DiceView1: UIImageView!
+    @IBOutlet weak var DiceView2: UIImageView!
+    var DiceArry=[#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func RollButton(_ sender: UIButton) {
+        DiceView1.image=DiceArry.randomElement()
+        DiceView2.image=DiceArry.randomElement()
+    }
 }
 
